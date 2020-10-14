f743885188e595014e846db889d061a95397aa3a
stefano <s.kouroupis@gmail.com>
2020-10-14 11:51:16 +0100

    frame in dxf

--- src/exported_geometry.rs
+++ src/exported_geometry.rs
@@ -6,12 +6,14 @@ use dxf::Point;
 pub fn get_geometry(config: &Layout) -> Vec<Polyline> {
     let mut polylines: Vec<Polyline> = vec![];
     let spacing = 190.;
+    let y_starting_point = 500.;
     let mut y_position = 500.;
-
+    let mut minimum_board_width = 0.;
+    let mut row_num: i32 = 0;
+    let border = 50.0;
     for row in &config.layout {
-        let horizontal_offset_in_units = 0.0;
-        let mut x_position = 0.0;
-
+        let mut x_position = border;
+        row_num += 1;
         for key in row {
             // this is used for keys bigger than 1U to be placed correctly
             // for 1U keys this results to 0
@@ -29,11 +31,52 @@ pub fn get_geometry(config: &Layout) -> Vec<Polyline> {
                 }
             }
             x_position += 190.5 + offset * 2.;
+
+            if row_num == 1 {
+                minimum_board_width = minimum_board_width + spacing * key.size;
+            }
         }
         y_position -= spacing as f64 + 0.5;
     }
 
+    polylines.push(frame(
+        minimum_board_width as f64,
+        (((row_num as f32 * spacing) + ((row_num - 1) as f32 * 0.5)) - 50.) as f64,
+        border,
+        y_starting_point,
+        border,
+    ));
+
     return polylines;
+}
+
+fn frame(
+    minimum_board_width: f64,
+    minimum_board_height: f64,
+    x: f64,
+    y: f64,
+    border: f64,
+) -> Polyline {
+    let mut polyline = Polyline::default();
+
+    polyline.vertices = [
+        Vertex::new(Point::new(x - border, y + border, 0.)),
+        Vertex::new(Point::new(x + minimum_board_width + border, y + border, 0.)),
+        Vertex::new(Point::new(
+            x + minimum_board_width + border,
+            y - (minimum_board_height + border),
+            0.,
+        )),
+        Vertex::new(Point::new(
+            x - border,
+            y - (minimum_board_height + border),
+            0.,
+        )),
+        Vertex::new(Point::new(x - border, y + border, 0.)),
+    ]
+    .to_vec();
+
+    polyline
 }
 
 fn switch(x: f64, y: f64) -> Polyline {
